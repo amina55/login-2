c87f7d79df93dd37b1eba9231d0d0d75b03fdb4a
Macbook Po <Mac@Macbooks-MacBook-Pro.local>
2017-05-19 20:42:49 +0500

    remove extra logs

--- database_access.php
+++ database_access.php
@@ -5,5 +5,5 @@ $dbpass = 'root';
 $dbhost = 'localhost';
 $dbname='test';
 
-$connection = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, [PDO::ATTR_PERSISTENT => true]);
+$connection = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 ?>
